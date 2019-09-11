2d710b3b39e4c4520f82674eb18fcaade17484ee
Grant Isom <grant.isom@gmail.com>
2019-09-11 15:52:10 -0500

    Initial Commit

---
+++ ContactCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactCard
+//
+//  Created by Grant Isom on 9/11/19.
+//  Copyright © 2019 Grant Isom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
